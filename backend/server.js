607302c81901ee64e0a3ae27a97f4008f5056002
surajgoswami03 <surajgoswami7866@gmail.com>
2025-06-01 12:36:51 +0530

    first init

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`server is running on PORT:${PORT}`);
+});
